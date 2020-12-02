03858df48c158cc91888b55c13f496a75a53a90f
Nghia Nguyen Duc Dai <nghia.nguyenduc@axonactive.com>
2020-12-02 17:12:03 +0700

    enable swagger

--- src/main/java/dn/com/user/service/config/WebSecurityConfig.java
+++ src/main/java/dn/com/user/service/config/WebSecurityConfig.java
@@ -27,6 +27,14 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -59,6 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS ).and()
                 .exceptionHandling().authenticationEntryPoint( restAuthenticationEntryPoint ).and()
                 .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers(
                         HttpMethod.GET,
                         "/",
